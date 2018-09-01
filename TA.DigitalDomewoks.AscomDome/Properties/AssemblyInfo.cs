c4f728c6a9518cb088fa6064f52e83d53153ec38
Tim Long <Tim@tigranetworks.co.uk>
2018-09-01 18:14:14 +0100

    Add all code, only the installer left to do.

---
+++ TA.DigitalDomewoks.AscomDome/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// This file is part of the TA.DigitalDomeworks project
+// 
+// Copyright © 2016-2018 Tigra Astronomy, all rights reserved.
+// 
+// File: AssemblyInfo.cs  Last modified: 2018-09-01@17:52 by Tim Long
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TA.DigitalDomewoks.AscomDome")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2067d7e2-410a-4b8b-9d07-425d27bace10")]
---
+++ TA.DigitalDomeworks.AscomSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// This file is part of the TA.DigitalDomeworks project
+// 
+// Copyright © 2016-2018 Tigra Astronomy, all rights reserved.
+// 
+// File: AssemblyInfo.cs  Last modified: 2018-08-30@21:27 by Tim Long
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("TA.DigitalDomeworks.AscomSwitch")]
+[assembly: AssemblyDescription("ASCOM Switch Driver for User GPIO")]
